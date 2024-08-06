ae218c095f8f3586993faed0964ba7c769bae534
Zheng Lu <zhenglu@Zhengs-MacBook-Pro.local>
2024-08-06 14:54:44 -0700

    deseralization completed

--- src/test/java/org/example/oAuthTest.java
+++ src/test/java/org/example/oAuthTest.java
@@ -1,7 +1,15 @@
 package org.example;
 
 import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pojo.Api;
+import pojo.GetCourse;
+import pojo.WebAutomation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 
@@ -34,5 +42,34 @@ public class oAuthTest {
                         .asString();
 
         System.out.println(getResponse);
+
+        // Deserialization
+        GetCourse getCourse = given()
+                .queryParam("access_token", token)
+                .when()
+                .get("https://rahulshettyacademy.com/oauthapi/getCourseDetails")
+                .as(GetCourse.class);
+        System.out.println(getCourse.getLinkedIn());
+        System.out.println(getCourse.getCourses().getApi().get(1).getCourseTitle());
+
+        List<Api> apiCourses = getCourse.getCourses().getApi();
+        for(int i=0; i<apiCourses.size(); i++) {
+            if(apiCourses.get(i).getCourseTitle().equalsIgnoreCase("SoapUI Webservices testing")) {
+                System.out.println(apiCourses.get(i).getPrice());
+            }
+        }
+
+        List<WebAutomation> webautomationCourses = getCourse.getCourses().getWebAutomation();
+        String[] courseTitles = {"Selenium Webdriver Java", "Cypress", "Protractor"};
+        for(int i=0; i<webautomationCourses.size(); i++) {
+            Assert.assertEquals(webautomationCourses.get(i).getCourseTitle(), courseTitles[i]);
+        }
+        List<String> actualTitles = new ArrayList<String>();
+        for(int i=0; i< webautomationCourses.size(); i++) {
+            actualTitles.add(webautomationCourses.get(i).getCourseTitle());
+        }
+
+        Assert.assertTrue(actualTitles.equals(Arrays.asList(courseTitles)));
+        Assert.assertEquals(actualTitles, Arrays.asList(courseTitles));
     }
 }
---
+++ src/test/java/pojo/GetCourse.java
@@ -0,0 +1,60 @@
+package pojo;
+
+public class GetCourse {
+    // Deseralization
+    private String url;
+    private String services;
+    private String expertise;
+    // Courses contains mini-JSON (nested JSON), and then array
+    private Courses Courses;
+    private String instructor;
+    private String linkedIn;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getServices() {
+        return services;
+    }
+
+    public void setServices(String services) {
+        this.services = services;
+    }
+
+    public String getExpertise() {
+        return expertise;
+    }
+
+    public void setExpertise(String expertise) {
+        this.expertise = expertise;
+    }
+
+    public Courses getCourses() {
+        return Courses;
+    }
+
+    public void setCourses(Courses courses) {
+        Courses = courses;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+}
